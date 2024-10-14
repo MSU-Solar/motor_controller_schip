ab6cf66f4f79aa0b00c7dd647c065d979147e60a
Vedant K. Naik <vnaik792014@gmail.com>
2024-10-14 12:14:51 -0400

    Update clang_format.sh

--- tools/clang_format.sh
+++ tools/clang_format.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
 
-# Run clang-format on all .c, .cpp, .h, and .hpp files in the specified directory
-SRC_DIR="motor_controller/motor_controller/Src"
-find $SRC_DIR -regex '.*\.\(c\|cpp\|h\|hpp\)' -exec clang-format -i {} \;
-
-# Add a success message
-echo "Code formatted using clang-format in $SRC_DIR."
